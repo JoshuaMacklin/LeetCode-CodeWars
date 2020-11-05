405d1c6911a26a4a67022859a788661ddb2b7876
Joshua Macklin <69019114+JoshuaMacklin@users.noreply.github.com>
2020-11-05 11:10:50 -0500

    Update Remove consecutive duplicate words.js

--- Remove consecutive duplicate words.js
+++ Remove consecutive duplicate words.js
@@ -4,12 +4,24 @@
 
 //--> "alpha beta gamma delta alpha beta gamma delta"
 
+//Attempt 1
 const removeConsecutiveDuplicates = s => {
   let arr = s.split(' ');
   let consecutiveArr = [];
   for(let i=0; i<arr.length; i++){
     if(arr[i] === arr[i+1]){
     }else{
+      consecutiveArr.push(arr[i]);
+    }
+  }return consecutiveArr.join(' ')
+}
+
+//Attempt 1 Clean Up
+const removeConsecutiveDuplicates = s => {
+  let arr = s.split(' ');
+  let consecutiveArr = [];
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] !== arr[i+1]){
       consecutiveArr.push(arr[i]);
     }
   }return consecutiveArr.join(' ')
