e855bb03d9d27e23157fb7ec2ec459fd19eebfd0
Josh Macklin <69019114+JoshuaMacklin@users.noreply.github.com>
2024-05-02 18:00:07 -0700

    Update Does_my_number_look_big_in_this.py

--- Does_my_number_look_big_in_this.py
+++ Does_my_number_look_big_in_this.py
@@ -1,4 +1,6 @@
 '''
+https://www.codewars.com/kata/5287e858c6b5a9678200083c/
+
 A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
 
 For example, take 153 (3 digits), which is narcissistic:
